0c643293fc72a87a10d6c25b5d330c5636e0d093
주현필 <wngusvlf@gmail.com>
2023-11-30 16:19:15 +0900

    test

--- src/main/java/Encrpytion/mapper/Mapper.java
+++ src/main/java/Encrpytion/mapper/Mapper.java
@@ -1,4 +1,12 @@
 package Encrpytion.mapper;
 
+import Encrpytion.domain.UserPhysicalInfo;
 public class Mapper {
+    public Mapper() {
+    }
+    // 나이 범주화 메서드
+    public Integer categorizeAge(UserPhysicalInfo userInfo) {
+        Integer age = userInfo.getAge();
+        return (age / 10) * 10;
+    }
 }
