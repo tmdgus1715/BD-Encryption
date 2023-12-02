957b3347228069b6fb84b979353fb0445041eaef
younggin446@naver.com <younggin446@naver.com>
2023-12-02 23:52:48 +0900

    나이를 기준으로 각 나이대별 신장과 평균 총계처리 Mapper,Reducer 코드 작성했습니다. 원래 다른 학우님이 나이 범주화하는 코드 작성하시기로 했는데, 작성하다보니 헷갈리는거 같아 그냥 나이대별 몸무게와 신장 총계처리하는 코드를 작성했습니다.

--- src/main/java/Encrpytion/util/TotalProcessing.java
+++ src/main/java/Encrpytion/util/TotalProcessing.java
@@ -7,16 +7,19 @@ import org.apache.hadoop.mapreduce.*;
 public class TotalProcessing {
 
     public static void main(String[] args) throws Exception {
-        Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "Total Processing");
-        job.setJarByClass(TotalProcessing.class);
+        Configuration conf = new Configuratiogin(); // Hadoop 환경 구성하기 위한 Configuration 객체
+        Job job = Job.getInstance(conf, "Total Processing"); // Map-Reduce 작업 실행하기 위한 Job 객체 생성
+        job.setJarByClass(TotalProcessing.class); // jar파일의 클래스 선정
         
-        job.setMapperClass(MapperClass.class);
-        job.setReducerClass(ReducerClass.class);
+        // Mapper 클래스와 Reducer 클래스 매핑
+        job.setMapperClass(Mapper.class);
+        job.setReducerClass(Reducer.class);
 
+         // 출력값 클래스 설정
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
-
+        
+         // 출력값 클래스 설정
         job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(IntWritable.class);
 
@@ -24,6 +27,8 @@ public class TotalProcessing {
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
+        // MapReduce 작업을 실행하고 완료될 때까지 대기합니다. 작업이 성공적으로 완료되면 프로그램은 0으로 종료됩니다.
+         // 그렇지 않은 경우 1로 종료됩니다.
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
