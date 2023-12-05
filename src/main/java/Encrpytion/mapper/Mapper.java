5c23f624410809dd628c859c3f18b944feafbce5
RaonTale <112318312+RaonTale@users.noreply.github.com>
2023-12-05 14:39:25 +0900

    Update Mapper.java
    총계처리에 사용된 범주화에 맞게 변형했습니다.
    출력값 string과 int로 해놔서 둘 중 하나 쓰면 될 것같습니다.

--- src/main/java/Encrpytion/mapper/Mapper.java
+++ src/main/java/Encrpytion/mapper/Mapper.java
@@ -1,12 +1,14 @@
 package Encrpytion.mapper;
 
-import Encrpytion.domain.UserPhysicalInfo;
 public class Mapper {
     public Mapper() {
     }
     // 나이 범주화 메서드
-    public Integer categorizeAge(UserPhysicalInfo userInfo) {
-        Integer age = userInfo.getAge();
+    private Integer categorizeAge_int(int age) {
         return (age % 10) * 10;
+    }
+    private String categorizeAge_string(int age) {
+        int categorizeAge = (age % 10) * 10;
+        return String.format("%d~%d", categorizeAge, categorizeAge +9);
     }
 }
